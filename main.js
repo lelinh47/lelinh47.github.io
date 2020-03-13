e44960e0dc159db77e666b213cefcc025c97b8ff
lelinh47 <61420305+lelinh47@users.noreply.github.com>
2020-03-13 14:52:02 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,29 @@ const socket = io('https://rtc-start-kit-full.herokuapp.com/');
 
 $('#div-chat').hide();
 
+let options = {
+    host: "global.xirsys.net",
+    path: "/_turn/lelinh47.github.io",
+    method: "PUT",
+    headers: {
+        "Authorization": "Basic " + Buffer.from("lelinh47:03451670-5711-11ea-ae83-0242ac110004").toString("base64"),
+        "Content-Type": "application/json",
+        "Content-Length": bodyString.length
+    }
+}
+
+$(function(){
+    // Get Xirsys ICE (STUN/TURN)
+    if(!ice){
+        ice = new $xirsys.ice('/webrtc');
+        ice.on(ice.onICEList, function (evt){
+            console.log('onICE ',evt);
+            if(evt.type == ice.onICEList){
+                create(ice.iceServers);
+            }
+        });
+    }
+});
 
 socket.on('DANH_SACH_ONLINE', arrUserInfo => {
     $('#div-chat').show();
@@ -36,7 +59,13 @@ function playStream(idVideoTag, stream) {
 // openStream()
 // .then(stream => playStream('localStream', stream));
 
-const peer = new Peer({key: 'peerjs', host: 'lelinh.herokuapp.com', secure: true, port: 443});
+const peer = new Peer({
+    key: 'peerjs', 
+    host: 'lelinh.herokuapp.com', 
+    secure: true, 
+    port: 443,
+    config: ice.iceServers
+});
 
 peer.on('open', id  => {
     $('#my-peer').append(id)
