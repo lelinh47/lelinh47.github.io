37092efb59d8c68fb2cd67be60022c7c022fc617
lelinh47 <61420305+lelinh47@users.noreply.github.com>
2020-03-17 14:41:37 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,26 @@ const socket = io('https://rtc-start-kit-full.herokuapp.com/');
 
 $('#div-chat').hide();
 
+let customConfig;
+
+$.ajax({
+  url: "https://service.xirsys.com/ice",
+  data: {
+    ident: "lelinh47",
+    secret: "03451670-5711-11ea-ae83-0242ac110004",
+    domain: "lelinh47.github.io",
+    application: "default",
+    room: "default",
+    secure: 1
+  },
+  success: function (data, status) {
+    // data.d is where the iceServers object lives
+    customConfig = data.d;
+    console.log(customConfig);
+  },
+  async: false
+});
+
 socket.on('DANH_SACH_ONLINE', arrUserInfo => {
     $('#div-chat').show();
     $('#div-dang-ky').hide(); 
@@ -35,101 +55,13 @@ function playStream(idVideoTag, stream) {
 // openStream()
 // .then(stream => playStream('localStream', stream));
 
-if(!$xirsys) var $xirsys = new Object();
-var _ice = $xirsys.ice = function (apiUrl, info) {
-    this.apiUrl = !!apiUrl ? apiUrl : '/webrtc';
-    //info can have TURN only filter.
-    //console.log('*ice*  constructor: ',this.apiUrl);
-    this.evtListeners = {};
-    this.iceServers;
-    if(!!this.apiUrl){
-        this.doICE();//first get our token.
-    }
-}
-
-_ice.prototype.onICEList = 'onICEList';
-
-_ice.prototype.doICE = function () {
-    console.log('*ice*  doICE: ',this.apiUrl);
-    var own = this;
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function($evt){
-        if(xhr.readyState == 4 && xhr.status == 200){
-            var res = JSON.parse(xhr.responseText);
-            console.log('*ice*  response: ',res);
-            own.iceServers = own.urlToUrls(res.v.iceServers);
-            
-            own.emit(own.onICEList);
-        }
-    }
-    xhr.open("PUT", this.apiUrl+"/_turn", true);
-    xhr.send();
-}
-
-//check for depricated RTCIceServer url property, needs to be urls now.
-_ice.prototype.urlToUrls = function(arr){
-    var l = arr.length, i;
-    for(i=0; i<l; i++){
-        var item = arr[i];
-        var v = item.url;
-        if(!!v){
-            item.urls = v;
-            delete item.url;
-        }
-    }
-    return arr;
-}
-
-_ice.prototype.on = function(sEvent,cbFunc){
-    //console.log('*ice*  on ',sEvent);
-    if(!sEvent || !cbFunc) {
-        console.log('error:  missing arguments for on event.');
-        return false;
-    }
-    if(!this.evtListeners[sEvent]) this.evtListeners[sEvent] = [];
-    this.evtListeners[sEvent].push(cbFunc);
-}
-_ice.prototype.off = function(sEvent,cbFunc){
-    //console.log('off');
-    this.evtListeners.push(cbFunc);
-}
-
-_ice.prototype.emit = function(sEvent, data){
-    var handlers = this.evtListeners[sEvent];
-    if(!!handlers) {
-        var l = handlers.length;
-        for(var i=0; i<l; i++){
-            var item = handlers[i];
-            item.apply(this,[{type:this.onICEList}]);
-        }
-    }
-}
-
-console.log('$xirsys.ice Loaded Successfuly!!!');
-_ice = null;
-
-let ice;
-let iceServers;
-
-$(function(){
-    // Get Xirsys ICE (STUN/TURN)
-    if(!ice){
-        ice = new $xirsys.ice('/webrtc');
-        ice.on(ice.onICEList, function (evt){
-            console.log('onICE ',evt);
-            if(evt.type == ice.onICEList){
-                create(ice.iceServers);
-            }
-        });
-    }
-});
 
 const peer = new Peer({
     key: 'peerjs', 
     host: 'lelinh.herokuapp.com', 
     secure: true, 
     port: 443,
-    config: ice.iceServers
+    config: customConfig
 });
 
 peer.on('open', id  => {
